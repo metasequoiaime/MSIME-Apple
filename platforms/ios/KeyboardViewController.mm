aa41cbaa78313d0e6b2f011ae54505a1a9fdd415
Evan Hu <e-hu@unext.jp>
2026-09-10 12:05:09 +0900

    feat(ios): add minimal keyboard controls

--- platforms/ios/KeyboardViewController.mm
+++ platforms/ios/KeyboardViewController.mm
@@ -20,6 +20,60 @@
     [super viewDidLoad];
     self.session = [[MSIMEClientSession alloc] initWithOptions:[self runtimeOptions] error:nil];
     [self apply:[self.session setFocused:YES error:nil]];
+    [self buildKeyboard];
+}
+
+- (void)buildKeyboard {
+    UIStackView *rows = [[UIStackView alloc] initWithFrame:CGRectZero];
+    rows.axis = UILayoutConstraintAxisVertical;
+    rows.spacing = 6;
+    rows.translatesAutoresizingMaskIntoConstraints = NO;
+    NSArray *keys = @[@[@"q", @"w", @"e", @"r", @"t", @"y", @"u", @"i", @"o", @"p"],
+                     @[@"a", @"s", @"d", @"f", @"g", @"h", @"j", @"k", @"l"],
+                     @[@"z", @"x", @"c", @"v", @"b", @"n", @"m"]];
+    for (NSArray *rowKeys in keys) {
+        UIStackView *row = [[UIStackView alloc] initWithFrame:CGRectZero];
+        row.axis = UILayoutConstraintAxisHorizontal;
+        row.distribution = UIStackViewDistributionFillEqually;
+        row.spacing = 4;
+        for (NSString *key in rowKeys) {
+            UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+            [button setTitle:key.uppercaseString forState:UIControlStateNormal];
+            button.backgroundColor = UIColor.secondarySystemBackgroundColor;
+            [button addTarget:self action:@selector(keyPressed:) forControlEvents:UIControlEventTouchUpInside];
+            button.accessibilityLabel = key;
+            [row addArrangedSubview:button];
+        }
+        [rows addArrangedSubview:row];
+    }
+    UIStackView *actions = [[UIStackView alloc] initWithFrame:CGRectZero];
+    actions.axis = UILayoutConstraintAxisHorizontal;
+    actions.distribution = UIStackViewDistributionFillEqually;
+    actions.spacing = 4;
+    for (NSString *title in @[@"空格", @"⌫", @"回车"]) {
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+        [button setTitle:title forState:UIControlStateNormal];
+        button.backgroundColor = UIColor.tertiarySystemBackgroundColor;
+        [button addTarget:self action:@selector(actionPressed:) forControlEvents:UIControlEventTouchUpInside];
+        [actions addArrangedSubview:button];
+    }
+    [rows addArrangedSubview:actions];
+    [self.view addSubview:rows];
+    [NSLayoutConstraint activateConstraints:@[
+        [rows.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:6],
+        [rows.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:-6],
+        [rows.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:6],
+        [rows.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:-6]]];
+}
+
+- (void)keyPressed:(UIButton *)button {
+    [self handleCharacter:button.accessibilityLabel];
+}
+
+- (void)actionPressed:(UIButton *)button {
+    if ([button.currentTitle isEqualToString:@"空格"]) [self apply:[self.session command:MSIME_COMMIT_CANDIDATE error:nil]];
+    else if ([button.currentTitle isEqualToString:@"⌫"]) [self deleteBackward];
+    else [self apply:[self.session command:MSIME_COMMIT_RAW error:nil]];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
