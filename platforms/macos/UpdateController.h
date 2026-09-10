a5753052d0c4f701f104ddc458ed03739184a614
Evan Hu <e-hu@unext.jp>
2026-09-11 08:19:46 +0900

    fix(macos): annotate update controller nullability

--- platforms/macos/UpdateController.h
+++ platforms/macos/UpdateController.h
@@ -1,7 +1,9 @@
 #pragma once
 #import <AppKit/AppKit.h>
+NS_ASSUME_NONNULL_BEGIN
 @interface MSIMEUpdateController : NSObject
 + (instancetype)sharedController;
 - (BOOL)canCheckForUpdates;
 - (void)checkForUpdates:(nullable id)sender;
 @end
+NS_ASSUME_NONNULL_END
