ab3c728630a0b3377cf77403f4abd3962316ab82
Evan Hu <e-hu@unext.jp>
2026-09-10 14:04:40 +0900

    feat(ios): add numeric and punctuation keys

--- platforms/ios/KeyboardViewController.mm
+++ platforms/ios/KeyboardViewController.mm
@@ -70,9 +70,11 @@
     rows.axis = UILayoutConstraintAxisVertical;
     rows.spacing = 6;
     rows.translatesAutoresizingMaskIntoConstraints = NO;
-    NSArray *keys = @[@[@"q", @"w", @"e", @"r", @"t", @"y", @"u", @"i", @"o", @"p"],
+    NSArray *keys = @[@[@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"0"],
+                     @[@"q", @"w", @"e", @"r", @"t", @"y", @"u", @"i", @"o", @"p"],
                      @[@"a", @"s", @"d", @"f", @"g", @"h", @"j", @"k", @"l"],
-                     @[@"z", @"x", @"c", @"v", @"b", @"n", @"m"]];
+                     @[@"z", @"x", @"c", @"v", @"b", @"n", @"m"],
+                     @[@",", @".", @"?", @"!", @"'"]];
     for (NSArray *rowKeys in keys) {
         UIStackView *row = [[UIStackView alloc] initWithFrame:CGRectZero];
         row.axis = UILayoutConstraintAxisHorizontal;
