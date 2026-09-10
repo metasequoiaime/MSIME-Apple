696c45467d259f35c96d770e0b042eff5b796d5f
Evan Hu <e-hu@unext.jp>
2026-09-10 14:16:44 +0900

    feat(ios): reload shared preferences on session start

--- platforms/ios/KeyboardViewController.mm
+++ platforms/ios/KeyboardViewController.mm
@@ -58,6 +58,15 @@
     self.session = [[MSIMEClientSession alloc] initWithOptions:[self runtimeOptions] error:&error];
     if (!self.session) return;
     [self apply:[self.session setFocused:YES error:&error]];
+    NSString *directory = [self runtimeOptions][@"preferences_directory"];
+    if ([directory isKindOfClass:NSString.class] && directory.length) {
+        __weak MSIMEKeyboardViewController *weakSelf = self;
+        [self.session reloadPreferencesDirectory:directory completion:^(NSDictionary *result, NSError *reloadError) {
+            MSIMEKeyboardViewController *strongSelf = weakSelf;
+            if (!strongSelf || reloadError || !result) return;
+            [strongSelf apply:result];
+        }];
+    }
 }
 
 - (void)viewWillAppear:(BOOL)animated {
