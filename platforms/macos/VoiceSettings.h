6f01fa345051aacaac273d9eba20c4c9a2bc4228
Evan <e-hu@unext.jp>
2026-09-11 16:48:07 +0900

    feat(macos): add voice settings storage

--- platforms/macos/VoiceSettings.h
+++ platforms/macos/VoiceSettings.h
@@ -1,7 +1,10 @@
 #pragma once
-#import <AppKit/AppKit.h>
-FOUNDATION_EXPORT NSNotificationName const MSIMEVoiceSettingsDidChangeNotification;
-@interface MSIMEVoiceSettings : NSWindowController
-+ (instancetype)sharedSettings;
-- (void)showAndActivate;
+#import <Foundation/Foundation.h>
+@interface MetasequoiaVoiceSettings : NSObject
+@property(nonatomic,copy) NSString *provider, *endpoint, *model, *token;
+@property(nonatomic,copy) NSString *modelPath, *polishEndpoint, *polishModel, *polishToken;
+@property(nonatomic) BOOL polishEnabled;
++ (instancetype)loadSettings;
+- (BOOL)validate:(NSError **)error;
+- (BOOL)save:(NSError **)error;
 @end
