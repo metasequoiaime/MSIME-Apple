ab4dc0ff233eca2beb02f78e57c5e7ddfa1f53ed
Evan <e-hu@unext.jp>
2026-09-11 15:09:49 +0900

    feat(macos): add account token storage (#574)

--- platforms/macos/AccountKeychain.h
+++ platforms/macos/AccountKeychain.h
@@ -1,3 +1,4 @@
 #pragma once
 #import <Foundation/Foundation.h>
 FOUNDATION_EXPORT NSString *MSIMEKeychainToken(NSString *accountID, NSError **error);
+FOUNDATION_EXPORT BOOL MSIMEStoreKeychainToken(NSString *accountID, NSString *token, NSError **error);
--- platforms/macos/AccountKeychain.mm
+++ platforms/macos/AccountKeychain.mm
@@ -7,3 +7,11 @@ NSString *MSIMEKeychainToken(NSString *accountID, NSError **error) {
     if (status != errSecSuccess) { if (error) *error = [NSError errorWithDomain:NSOSStatusErrorDomain code:status userInfo:nil]; return nil; }
     return [[NSString alloc] initWithData:CFBridgingRelease(result) encoding:NSUTF8StringEncoding];
 }
+
+BOOL MSIMEStoreKeychainToken(NSString *accountID, NSString *token, NSError **error) {
+    if (accountID.length == 0 || token.length == 0 || [accountID rangeOfCharacterFromSet:[NSCharacterSet controlCharacterSet]].location != NSNotFound) { if (error) *error = [NSError errorWithDomain:@"MSIMEAccount" code:400 userInfo:nil]; return NO; }
+    NSData *data = [token dataUsingEncoding:NSUTF8StringEncoding]; NSDictionary *query = @{(__bridge id)kSecClass:(__bridge id)kSecClassGenericPassword,(__bridge id)kSecAttrService:@"com.metasequoia.msime.account",(__bridge id)kSecAttrAccount:accountID};
+    OSStatus status = SecItemUpdate((__bridge CFDictionaryRef)query, (__bridge CFDictionaryRef)@{(__bridge id)kSecValueData:data});
+    if (status == errSecItemNotFound) { NSMutableDictionary *item = [query mutableCopy]; item[(__bridge id)kSecValueData] = data; status = SecItemAdd((__bridge CFDictionaryRef)item, NULL); }
+    if (status != errSecSuccess) { if (error) *error = [NSError errorWithDomain:@"MSIMEAccount" code:status userInfo:nil]; return NO; } return YES;
+}
