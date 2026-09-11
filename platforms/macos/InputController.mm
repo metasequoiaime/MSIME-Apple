36eb3b8080e57e23b47c95ff834d31cbee59d1ac
Evan <e-hu@unext.jp>
2026-09-11 16:06:51 +0900

    feat(macos): add cloud clipboard menu entry (#606)

--- platforms/macos/InputController.mm
+++ platforms/macos/InputController.mm
@@ -10,6 +10,7 @@
 #import "AppearancePreferences.h"
 #import "PreferencesWindowController.h"
 #import "AccountWindowController.h"
+#import "CloudClipboardWindowController.h"
 #import "CandidateChrome.h"
 #include "CandidateSkin.h"
 #import "ChineseTextConversion.h"
@@ -153,6 +154,7 @@ static NSColor *SkinColor(msime::mac::Rgba color) {
     dictionary.target = self;
     [menu addItem:dictionary];
     NSMenuItem *account = [[NSMenuItem alloc] initWithTitle:@"账户状态…" action:@selector(showAccount:) keyEquivalent:@""]; account.target = self; [menu addItem:account];
+    NSMenuItem *clipboard = [[NSMenuItem alloc] initWithTitle:@"云剪贴板…" action:@selector(showCloudClipboard:) keyEquivalent:@""]; clipboard.target = self; [menu addItem:clipboard];
     NSMenuItem *prepare = [[NSMenuItem alloc] initWithTitle:@"准备词库…" action:@selector(prepareDictionary:) keyEquivalent:@""];
     prepare.target = self;
     [menu addItem:prepare];
@@ -172,6 +174,7 @@ static NSColor *SkinColor(msime::mac::Rgba color) {
     return menu;
 }
 - (void)showAccount:(id)sender { (void)sender; NSAlert *alert = [[NSAlert alloc] init]; alert.messageText = @"账户标识"; NSTextField *field = [[NSTextField alloc] initWithFrame:NSMakeRect(0,0,260,24)]; alert.accessoryView = field; [alert addButtonWithTitle:@"查看"]; [alert addButtonWithTitle:@"取消"]; if ([alert runModal] == NSAlertFirstButtonReturn && field.stringValue.length) [[MSIMEAccountWindowController sharedController] showForAccountID:field.stringValue]; }
+- (void)showCloudClipboard:(id)sender { (void)sender; NSAlert *alert = [[NSAlert alloc] init]; alert.messageText = @"账户 access token"; NSSecureTextField *field = [[NSSecureTextField alloc] initWithFrame:NSMakeRect(0,0,260,24)]; alert.accessoryView = field; [alert addButtonWithTitle:@"打开"]; [alert addButtonWithTitle:@"取消"]; if ([alert runModal] == NSAlertFirstButtonReturn && field.stringValue.length) [[MSIMECloudClipboardWindowController sharedController] showWithToken:field.stringValue]; }
 - (void)setEnglishInputMode:(BOOL)enabled {
     [self ensureAppearance];
     if (enabled && !_appearance.englishMode && _session && _activeClient) {
