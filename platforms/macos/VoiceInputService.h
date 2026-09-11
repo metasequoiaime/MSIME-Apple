0c296243e9c5b107a71ebae3f48910ef18926e9e
Evan Hu <e-hu@unext.jp>
2026-09-11 10:54:32 +0900

    feat(macos): add microphone audio capture

--- platforms/macos/VoiceInputService.h
+++ platforms/macos/VoiceInputService.h
@@ -5,9 +5,12 @@
 
 NS_ASSUME_NONNULL_BEGIN
 typedef void (^MSIMEVoiceInputResult)(NSDictionary * _Nullable result, NSError * _Nullable error);
+typedef void (^MSIMEVoiceAudioBuffer)(AVAudioPCMBuffer *buffer);
 @interface MSIMEVoiceInputService : NSObject
 - (BOOL)startWithSession:(MSIMEClientSession *)session generation:(uint64_t *)generation error:(NSError **)error;
 - (BOOL)cancelWithError:(NSError **)error;
+- (BOOL)startMicrophoneCapture:(MSIMEVoiceAudioBuffer)bufferHandler error:(NSError **)error;
+- (void)stopMicrophoneCapture;
 - (AVAuthorizationStatus)microphoneAuthorizationStatus;
 - (void)requestMicrophonePermission:(void (^)(BOOL granted))completion;
 - (void)applyText:(NSString *)text generation:(uint64_t)generation completion:(MSIMEVoiceInputResult)completion;
