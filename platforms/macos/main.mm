600f7d136111dc324d2111dd6dace6d39c93f3ee
Evan Hu <e-hu@unext.jp>
2026-09-11 09:24:45 +0900

    feat(macos): add standalone preferences launch

--- platforms/macos/main.mm
+++ platforms/macos/main.mm
@@ -1,6 +1,15 @@
 #import <AppKit/AppKit.h>
 #import <InputMethodKit/InputMethodKit.h>
 #import "InputSourceRegistration.h"
+#import "AppearancePreferences.h"
+#include <cstring>
+
+static bool MSIMEShouldShowPreferences(int argc, const char *argv[]) {
+    for (int index = 1; index < argc; ++index) {
+        if (strcmp(argv[index], "--preferences") == 0) return true;
+    }
+    return false;
+}
 
 int main(int argc, const char *argv[]) {
     @autoreleasepool {
@@ -15,6 +24,13 @@ int main(int argc, const char *argv[]) {
             return status == noErr ? 0 : 1;
         }
         [NSApplication sharedApplication];
+        if (MSIMEShouldShowPreferences(argc, argv)) {
+            [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+            [[MSIMEAppearancePreferences sharedPreferences] showWindow:nil];
+            [NSApp activateIgnoringOtherApps:YES];
+            [NSApp run];
+            return 0;
+        }
         __attribute__((objc_precise_lifetime)) IMKServer *server = [[IMKServer alloc] initWithName:@"MSIMEClientPreviewConnection" bundleIdentifier:NSBundle.mainBundle.bundleIdentifier];
         if (!server) return 1;
         [NSApp run];
