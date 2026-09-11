612f592c416bb86e5c6b6cb36d18b76e5090d899
Evan Hu <e-hu@unext.jp>
2026-09-11 10:47:51 +0900

    feat(macos): expose microphone permission for voice input

--- platforms/macos/VoiceInputService.h
+++ platforms/macos/VoiceInputService.h
@@ -1,5 +1,6 @@
 #pragma once
 #import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
 #import "MSIMEClientSession.h"
 
 NS_ASSUME_NONNULL_BEGIN
@@ -7,6 +8,8 @@ typedef void (^MSIMEVoiceInputResult)(NSDictionary * _Nullable result, NSError *
 @interface MSIMEVoiceInputService : NSObject
 - (BOOL)startWithSession:(MSIMEClientSession *)session generation:(uint64_t *)generation error:(NSError **)error;
 - (BOOL)cancelWithError:(NSError **)error;
+- (AVAuthorizationStatus)microphoneAuthorizationStatus;
+- (void)requestMicrophonePermission:(void (^)(BOOL granted))completion;
 - (void)applyText:(NSString *)text generation:(uint64_t)generation completion:(MSIMEVoiceInputResult)completion;
 @property(nonatomic, readonly, getter=isActive) BOOL active;
 @end
--- platforms/macos/VoiceInputService.mm
+++ platforms/macos/VoiceInputService.mm
@@ -1,5 +1,8 @@
 #import "VoiceInputService.h"
+#import <AVFoundation/AVFoundation.h>
 @implementation MSIMEVoiceInputService { __weak MSIMEClientSession *_session; BOOL _active; }
+- (AVAuthorizationStatus)microphoneAuthorizationStatus { return [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeAudio]; }
+- (void)requestMicrophonePermission:(void (^)(BOOL))completion { [AVCaptureDevice requestAccessForMediaType:AVMediaTypeAudio completionHandler:^(BOOL granted) { dispatch_async(dispatch_get_main_queue(), ^{ completion(granted); }); }]; }
 - (BOOL)isActive { return _active; }
 - (BOOL)startWithSession:(MSIMEClientSession *)session generation:(uint64_t *)generation error:(NSError **)error { if (_active) return YES; NSDictionary *result = [session startVoiceWithError:error]; if (!result) return NO; _session = session; _active = YES; if (generation) *generation = [result[@"generation"] unsignedLongLongValue]; return YES; }
 - (BOOL)cancelWithError:(NSError **)error { if (!_active) return YES; BOOL ok = [_session cancelVoiceWithError:error]; _active = NO; _session = nil; return ok; }
