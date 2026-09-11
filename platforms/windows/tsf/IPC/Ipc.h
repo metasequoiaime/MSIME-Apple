ab15eb3ca3bc61801bf4ae69dec58e5607bc5bdb
Evan Hu <e-hu@unext.jp>
2026-09-12 03:21:00 +0900

    fix(windows-tsf): bind IPC to shared contracts

--- platforms/windows/tsf/IPC/Ipc.h
+++ platforms/windows/tsf/IPC/Ipc.h
@@ -9,7 +9,7 @@
 #include <string>
 #include <vector>
 
-#include "../../../vendor/MetasequoiaImeEngine/contracts/windows_ipc.h"
+#include "../../../../vendor/MSIME-Engine/contracts/windows_ipc.h"
 
 int InitIpc();
 int InitNamedpipe();
