bcd48c280ba16850d6b03bf450bfcc202216fcd4
Evan Hu <e-hu@unext.jp>
2026-09-12 04:29:11 +0900

    fix(macos): wire dictionary runtime engine headers

--- platforms/macos/ChineseTextConversion.h
+++ platforms/macos/ChineseTextConversion.h
@@ -2,4 +2,3 @@
 #import <Foundation/Foundation.h>
 FOUNDATION_EXPORT NSString *MetasequoiaChineseOutputString(NSString *text, BOOL traditionalOutput);
 #define MSIMEChineseOutputString MetasequoiaChineseOutputString
-#define MSIMEScriptConversionApplies MetasequoiaScriptConversionApplies
