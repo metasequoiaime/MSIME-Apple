da766f24fc80858dc63af6b579193a9f8c393244
Evan <e-hu@unext.jp>
2026-09-11 16:40:36 +0900

    feat(macos): add traditional Chinese output conversion

--- platforms/macos/ChineseTextConversion.h
+++ platforms/macos/ChineseTextConversion.h
@@ -1,6 +1,3 @@
 #pragma once
 #import <Foundation/Foundation.h>
-
-// Based on MSIME-Apple b637828e15eafcb5e459edd270a962dd14517285.
-FOUNDATION_EXPORT NSString *MSIMEChineseOutputString(NSString *text, BOOL traditionalOutput);
-FOUNDATION_EXPORT BOOL MSIMEScriptConversionApplies(NSDictionary *context);
+FOUNDATION_EXPORT NSString *MetasequoiaChineseOutputString(NSString *text, BOOL traditionalOutput);
