41c4c2c55dcf64508b2e496ceb5ef0202defeb8f
Evan <e-hu@unext.jp>
2026-09-12 02:12:40 +0900

    feat(macos): add update controller bridge
    * feat(macos): add string conversion helpers
    
    * feat(macos): add update controller bridge

--- platforms/macos/UpdateController.mm
+++ platforms/macos/UpdateController.mm
@@ -1,15 +1,91 @@
 #import "UpdateController.h"
 
-@implementation MSIMEUpdateController
-+ (instancetype)sharedController {
-    static MSIMEUpdateController *controller;
-    static dispatch_once_t once;
-    dispatch_once(&once, ^{ controller = [self new]; });
+#import <Sparkle/Sparkle.h>
+
+@interface MetasequoiaSparkleUpdateDriver : NSObject <MetasequoiaUpdateDriver>
+@property(nonatomic, readonly) SPUStandardUpdaterController *updaterController;
+@end
+
+@implementation MetasequoiaSparkleUpdateDriver
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self != nil)
+    {
+        _updaterController = [[SPUStandardUpdaterController alloc] initWithStartingUpdater:YES
+                                                                           updaterDelegate:nil
+                                                                        userDriverDelegate:nil];
+    }
+    return self;
+}
+
+- (BOOL)canCheckForUpdates
+{
+    return self.updaterController.updater.canCheckForUpdates;
+}
+
+- (BOOL)automaticallyChecksForUpdates
+{
+    return self.updaterController.updater.automaticallyChecksForUpdates;
+}
+
+- (void)checkForUpdates:(id)sender
+{
+    [self.updaterController checkForUpdates:sender];
+}
+
+@end
+
+@interface MetasequoiaUpdateController ()
+@property(nonatomic) id<MetasequoiaUpdateDriver> driver;
+@property(nonatomic, copy) MetasequoiaUpdateActivationHandler activationHandler;
+@end
+
+@implementation MetasequoiaUpdateController
+
++ (instancetype)sharedController
+{
+    static MetasequoiaUpdateController *controller = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+      MetasequoiaSparkleUpdateDriver *driver = [[MetasequoiaSparkleUpdateDriver alloc] init];
+      controller =
+          [[MetasequoiaUpdateController alloc] initWithDriver:driver
+                                            activationHandler:^{
+                                              [NSApp setActivationPolicy:NSApplicationActivationPolicyAccessory];
+                                              [NSApp activateIgnoringOtherApps:YES];
+                                            }];
+    });
     return controller;
 }
-- (BOOL)canCheckForUpdates { return YES; }
-- (void)checkForUpdates:(id)sender {
-    (void)sender;
-    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"https://github.com/metasequoiaime/MSIME-Client/releases"]];
+
+- (instancetype)initWithDriver:(id<MetasequoiaUpdateDriver>)driver
+             activationHandler:(MetasequoiaUpdateActivationHandler)activationHandler
+{
+    self = [super init];
+    if (self != nil)
+    {
+        _driver = driver;
+        _activationHandler = [activationHandler copy];
+    }
+    return self;
 }
+
+- (BOOL)canCheckForUpdates
+{
+    return self.driver.canCheckForUpdates;
+}
+
+- (BOOL)automaticallyChecksForUpdates
+{
+    return self.driver.automaticallyChecksForUpdates;
+}
+
+- (void)checkForUpdates:(id)sender
+{
+    self.activationHandler();
+    [self.driver checkForUpdates:sender];
+}
+
 @end
