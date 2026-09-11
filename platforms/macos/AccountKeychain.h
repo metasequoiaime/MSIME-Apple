80fefc3af32fd5470faf30c226f8ff481d05a8f5
Evan <e-hu@unext.jp>
2026-09-11 15:32:04 +0900

    feat(macos): persist refresh tokens separately (#584)

--- platforms/macos/AccountKeychain.h
+++ platforms/macos/AccountKeychain.h
@@ -3,3 +3,5 @@
 FOUNDATION_EXPORT NSString *MSIMEKeychainToken(NSString *accountID, NSError **error);
 FOUNDATION_EXPORT BOOL MSIMEStoreKeychainToken(NSString *accountID, NSString *token, NSError **error);
 FOUNDATION_EXPORT BOOL MSIMERemoveKeychainToken(NSString *accountID, NSError **error);
+FOUNDATION_EXPORT NSString *MSIMEKeychainRefreshToken(NSString *accountID, NSError **error);
+FOUNDATION_EXPORT BOOL MSIMEStoreKeychainRefreshToken(NSString *accountID, NSString *token, NSError **error);
