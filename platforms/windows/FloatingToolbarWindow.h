4366054a15fc397aa59e1653b40630fa1443f028
Evan <suzukaze.haduki@gmail.com>
2026-09-13 06:32:28 +0800

    feat(windows): open about from floating toolbar
    Merge toolbar about action.

--- platforms/windows/FloatingToolbarWindow.h
+++ platforms/windows/FloatingToolbarWindow.h
@@ -20,6 +20,7 @@ public:
   void set_handwriting_action(Action action) { handwriting_action_ = std::move(action); }
   void set_keyboard_action(Action action) { keyboard_action_ = std::move(action); }
   void set_voice_action(Action action) { voice_action_ = std::move(action); }
+  void set_about_action(Action action) { about_action_ = std::move(action); }
   FloatingToolbarWindow(const FloatingToolbarWindow &) = delete;
   FloatingToolbarWindow &operator=(const FloatingToolbarWindow &) = delete;
   void refresh(bool enabled);
@@ -46,6 +47,7 @@ private:
   Action handwriting_action_;
   Action keyboard_action_;
   Action voice_action_;
+  Action about_action_;
   HWND window_ = nullptr;
   std::optional<ModePresentation> shown_;
   bool failed_ = false;
--- platforms/windows/main.cpp
+++ platforms/windows/main.cpp
@@ -218,6 +218,10 @@ int wmain(int argc, wchar_t **argv) {
       const auto request = shell_surface_request(TrayMenuCommand::ToggleVoiceInput);
       if (shell && request) (void)launch_shell_surface(*shell, *request);
     });
+    toolbar.set_about_action([&] {
+      const auto request = shell_surface_request(TrayMenuCommand::OpenAbout);
+      if (shell && request) (void)launch_shell_surface(*shell, *request);
+    });
     TrayMenuCapabilities menu_capabilities;
     menu_capabilities.emoji_panel = shell.has_value();
     menu_capabilities.handwriting_panel = shell.has_value();
