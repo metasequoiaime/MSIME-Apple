f7248b8e908c04a3873b5d068ff41e49905a697d
Evan <suzukaze.haduki@gmail.com>
2026-09-14 13:02:46 +0800

    feat(macos): route handwriting to tauri (#2133)

--- platforms/macos/InputController.mm
+++ platforms/macos/InputController.mm
@@ -797,6 +797,8 @@ static CGFloat MSIMEPreeditSlotWidth(void *) { return MSIMEPreeditCaretGap; }
 }
 - (void)showHandwriting:(id)sender {
     (void)sender;
+    NSURL *url = [[NSWorkspace sharedWorkspace] URLForApplicationWithBundleIdentifier:@"app.msime.client.preview"];
+    if (url) { NSWorkspaceOpenConfiguration *c = [NSWorkspaceOpenConfiguration new]; c.arguments = @[@"--route=handwriting"]; [[NSWorkspace sharedWorkspace] openApplicationAtURL:url configuration:c completionHandler:nil]; return; }
     Class bridge = NSClassFromString(@"MSIMEBackendWindowBridge");
     id shared = [bridge respondsToSelector:@selector(shared)] ? [bridge performSelector:@selector(shared)] : nil;
     if (![shared respondsToSelector:@selector(showHandwriting)]) { [self showAccount:nil]; return; }
