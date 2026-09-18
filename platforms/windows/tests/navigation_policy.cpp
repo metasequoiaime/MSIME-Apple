c15ecdabbb608f2309af9ba8805de8616734d792
Evan Hu <e-hu@unext.jp>
2026-09-18 18:22:23 +0900

    test(windows): cover navigation modifier policy

--- platforms/windows/tests/navigation_policy.cpp
+++ platforms/windows/tests/navigation_policy.cpp
@@ -1,10 +1,24 @@
 #include "../src/input/NavigationPolicy.h"
 #include <cassert>
 int main() {
-  msime::windows::FanyImeNamedpipeData packet{};
+  FanyImeNamedpipeData packet{};
   packet.event_type = FanyImePipeEventType::KeyEvent;
   packet.keycode = 0xBD;
   msime::windows::NavigationBindings bindings{true, false, false, false, false, false, false};
   assert(msime::windows::navigation_action(packet, bindings, false).has_value());
   assert(!msime::windows::navigation_action(packet, bindings, false, true).has_value());
+
+  msime::windows::NavigationBindings tab_bindings{};
+  tab_bindings.tab = true;
+  packet.keycode = 0x09;
+  packet.modifiers_down = 0;
+  const auto next = msime::windows::navigation_action(packet, tab_bindings, false);
+  assert(next && next->reply == msime::windows::NavigationReply::NextPage);
+  packet.modifiers_down = 1;
+  const auto previous = msime::windows::navigation_action(packet, tab_bindings, false);
+  assert(previous && previous->reply == msime::windows::NavigationReply::PreviousPage);
+  packet.modifiers_down = 2;
+  assert(!msime::windows::navigation_action(packet, tab_bindings, false));
+  packet.modifiers_down = 4;
+  assert(!msime::windows::navigation_action(packet, tab_bindings, false));
 }
